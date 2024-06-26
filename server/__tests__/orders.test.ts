f7168a7109ab021b053668b423e3558d666aa94e
Henry Ball <henry.ball@fauna.com>
2024-06-26 16:02:15 -0700

    update cart item

--- server/__tests__/orders.test.ts
+++ server/__tests__/orders.test.ts
@@ -1,19 +1,29 @@
 import req from "supertest";
 import app from "../src/app";
+import { seedTestData } from "./seed";
 import { faunaClient } from "../src/fauna/fauna-client";
+import { Product } from "../src/routes/products/products.model";
+import { Customer } from "../src/routes/customers/customers.model";
 
 describe("Orders", () => {
+  let product: Product;
+  let customer: Customer;
+
+  beforeAll(async () => {
+    const { product: p, customer: c } = await seedTestData();
+    product = p;
+    customer = c;
+  });
+
   afterAll(async () => {
     // Clean up our connection to Fauna.
     faunaClient.close();
   });
 
-  describe("PUT /customers/:cutomerId/orders/:orderId/item", () => {
+  describe("POST /customers/:id/cart/item", () => {
     [{}, { productName: "Lava Lamp" }, { quantity: 10 }].forEach((payload) => {
       it(`returns a 400 if it receives and invalid payload: ${payload}`, async () => {
-        const res = await req(app)
-          .put("/customers/1/orders/2/item")
-          .send(payload);
+        const res = await req(app).post("/customers/1/cart/item").send(payload);
         expect(res.status).toEqual(400);
         expect(res.body).toEqual({
           reason: "You must provide a productName and quantity.",
@@ -21,13 +31,59 @@ describe("Orders", () => {
       });
     });
 
-    it("returns a 200 if it receives a valid payload", async () => {
-      const res = await req(app).put("/customers/2/orders/2/item").send({
-        productName: "Lava Lamp",
-        quantity: 10,
+    it("returns an error if the customer does not exist", async () => {
+      const res = await req(app)
+        .post("/customers/1234/cart/item")
+        .send({ productName: product.name, quantity: 1 });
+      expect(res.status).toEqual(400);
+      expect(res.body).toEqual({
+        reason: "Customer does not exist.",
       });
-      expect(res.status).toEqual(200);
-      expect(res.body.message).toEqual("Put Order Item");
+    });
+
+    it("returns an error if the product does not exist", async () => {
+      const res = await req(app)
+        .post(`/customers/${customer.id}/cart/item`)
+        .send({ productName: "Bogus Product", quantity: 10 });
+      expect(res.status).toEqual(400);
+      expect(res.body).toEqual({
+        reason: "Product does not exist.",
+      });
+    });
+
+    it("return an error if the quantity is invalid", async () => {
+      const res = await req(app)
+        .post(`/customers/${customer.id}/cart/item`)
+        .send({ productName: product.name, quantity: -1 });
+      expect(res.status).toEqual(400);
+      expect(res.body).toEqual({
+        reason: "Quantity must be a non-negative integer.",
+      });
+    });
+
+    it("returns an error if the product is out of stock", async () => {
+      const res = await req(app)
+        .post(`/customers/${customer.id}/cart/item`)
+        .send({ productName: product.name, quantity: 10000 });
+      expect(res.status).toEqual(400);
+      expect(res.body).toEqual({
+        reason: "Product does not have the requested quantity in stock.",
+      });
+    });
+
+    it("updates the cart with the new item", async () => {
+      // Add an item to the cart.
+      const firstReq = await req(app)
+        .post(`/customers/${customer.id}/cart/item`)
+        .send({ productName: product.name, quantity: 1 });
+      expect(firstReq.status).toEqual(200);
+      expect(firstReq.body.quantity).toEqual(1);
+      // Update the quantity of the item in the cart.
+      const secondReq = await req(app)
+        .post(`/customers/${customer.id}/cart/item`)
+        .send({ productName: product.name, quantity: 2 });
+      expect(secondReq.status).toEqual(200);
+      expect(secondReq.body.quantity).toEqual(2);
     });
   });
 });
--- server/src/routes/orders/orders.controller.ts
+++ server/src/routes/orders/orders.controller.ts
@@ -1,31 +1,47 @@
+import { fql } from "fauna";
 import { Request, Response, Router } from "express";
+import { faunaClient } from "../../fauna/fauna-client";
 
 const router = Router();
 
 /**
- * Put Order Item
- * @route {PUT} orders/:orderId/item
- * @param customerId
- * @param orderId
+ * Post Cart Item
+ * @route {POST} /customers/:id/cart/item
+ * @param id string
  * @bodyparam productName
  * @bodyparam quantity
  * @returns OrderItem
  */
-router.put(
-  "/customers/:customerId/orders/:orderId/item",
-  (req: Request, res: Response) => {
-    const { productName, quantity } = req.body;
+router.post("/customers/:id/cart/item", async (req: Request, res: Response) => {
+  const { id: customerId } = req.params;
+  const { productName, quantity } = req.body;
 
-    if (!productName || !quantity) {
+  if (!productName || !quantity) {
+    return res.status(400).send({
+      reason: "You must provide a productName and quantity.",
+    });
+  }
+
+  try {
+    const { data: cartItem } = await faunaClient.query(
+      fql`updateCartItem(${customerId}, ${productName}, ${quantity})`
+    );
+
+    // TODO: we will need to strip out internal fields from the response.
+    return res.status(200).send(cartItem);
+  } catch (error: any) {
+    // We defined several abort contitions in the updateCartItem UDF.
+    // Use them to return appropriate error messages.
+    if (error.abort) {
       return res.status(400).send({
-        reason: "You must provide a productName and quantity.",
+        reason: error.abort,
       });
     }
 
-    return res.json({
-      message: "Put Order Item",
+    return res.status(500).send({
+      reason: "An error occurred while trying to update the cart.",
     });
   }
-);
+});
 
 export default router;
