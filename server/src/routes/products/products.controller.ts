56cb9cd6c7a49e6e58d53ddf563f6fbae9cc56f0
Henry Ball <henry.ball@fauna.com>
2024-06-27 14:07:04 -0700

    dont auto format products controller

--- server/src/routes/products/products.controller.ts
+++ server/src/routes/products/products.controller.ts
@@ -1,11 +1,11 @@
-import { Request, Response, Router } from 'express';
+import { Request, Response, Router } from "express";
 
 const router = Router();
 
 router.get("/products", (req: Request, res: Response) => {
   return res.json({
-        message: "GET Products"
-    });
+    message: "GET Products",
+  });
 });
 
 export default router;
