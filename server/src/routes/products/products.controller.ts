332075989a411371da4b6959bbe5b1c9587215c5
Shadid12 <Shadid12@users.noreply.github.com>
2024-07-09 12:09:42 -0400

    Added indexed covered queries (#33)

--- server/src/routes/products/products.controller.ts
+++ server/src/routes/products/products.controller.ts
@@ -241,4 +241,55 @@ router.patch(
   }
 );
 
+/**
+ * Find products by price and inventory.
+ * @route {GET} /products/search
+ * @queryparam minPrice
+ * @queryparam maxPrice
+ */
+router.get(
+  "/products/by-price",
+  async (req: Request, res: Response) => {
+    // Extract the minPrice, maxPrice, minStock, and maxStock query parameters from the request.
+    const {
+      minPrice = 0,
+      maxPrice = 10000,
+      limit = 25,
+      nextToken = undefined,
+    } = req.query;
+
+    try {
+      /**
+       * This is an example of a covered query.  A covered query is a query where all fields
+       * returned are indexed fields. In this case, we are querying the Product collection
+       * for products with a price between minPrice and maxPrice. We are also limiting the
+       * number of results returned to the limit parameter. The query is covered because
+       * all fields returned are indexed fields. In this case, the fields returned are
+       * `name`, `description`, `price`, and `stock` are all indexed fields.
+       * Covered queries are typically faster and less expensive than uncovered queries,
+       * which require document reads. 
+       * Learn more about covered queries here: https://docs.fauna.com/fauna/current/learn/data_model/indexes#covered-queries
+       */
+      const query = fql`
+        Product.sortedByPriceLowToHigh({ from: ${Number(minPrice)}, to: ${Number(maxPrice) }})
+        .pageSize(${Number(limit)}) {
+          name,
+          description,
+          price,
+          stock
+        }
+      `;
+
+      // Execute the query and return the results to the user.
+      const { data: products } = await faunaClient.query<Page<Product>>(
+        nextToken ? fql`Set.paginate(${nextToken as string})` : query
+      );
+      return res.status(200).send({data: products.data, nextToken: products.after});
+    } catch (error: any) {
+      // Return a generic 500 if we encounter an unexpected error.
+      return res.status(500).send({ message: "Internal Server Error" });
+    }
+  }
+);
+
 export default router;
